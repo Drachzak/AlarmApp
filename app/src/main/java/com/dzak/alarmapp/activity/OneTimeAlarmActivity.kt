47234ee6961244d55ee618c39df4965201e90836
Drachzak <dedet.emang.ok@gmail.com>
2022-01-17 15:08:53 +0700

    Initial commit

---
+++ app/src/main/java/com/dzak/alarmapp/activity/OneTimeAlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.dzak.alarmapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dzak.alarmapp.R
+
+class OneTimeAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_time_alarm)
+    }
+}
---
+++ app/src/main/java/com/dzak/alarmapp/activity/RepeatingAlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.dzak.alarmapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dzak.alarmapp.R
+
+class RepeatingAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repeating_alarm)
+    }
+}
